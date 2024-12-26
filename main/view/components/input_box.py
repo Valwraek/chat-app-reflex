7707ede065e2a933235b96a355dbe97cd90a9c24
Valwraek <bardiarajabirajabi@gmail.com>
2024-12-26 20:09:35 +0100

    feat(componente): :sparkles: Actualizar el input para que acepte css puro

--- main/view/components/input_box.py
+++ main/view/components/input_box.py
@@ -1,5 +1,4 @@
 import reflex as rx
-from ..styles import chat_style as cs
 from main.controller.pregunta_respuesta import State_qa as sqa
 def input_box() -> rx.Component:
     return (
@@ -9,6 +8,6 @@ def input_box() -> rx.Component:
             on_change=sqa.set_question,
             on_key_down=sqa.handle_key_down,
             autofocus=True,
-            style= cs.input_style,
+            class_name="input"
             ),       
     )
