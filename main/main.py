824153d7c6d83baccad66f6e6b187c004225a3c9
Valwraek <bardiarajabirajabi@gmail.com>
2024-12-26 20:07:02 +0100

    feat(main): :sparkles: Actualizar el módulo main para reconocer archivo css y cambiar el tema del proyecto a oscuro

--- main/main.py
+++ main/main.py
@@ -3,8 +3,17 @@ from .view.chat_app import chat
 
 def index() -> rx.Component:
    return rx.container(
-      chat(),
+      rx.box(
+        chat(),
+      ),
       )
 
-app = rx.App()
+app = rx.App(
+   stylesheets=[
+        "/estilos_chat.css",
+    ],
+    theme=rx.theme(
+        appearance="dark",
+    )
+)
 app.add_page(index)
