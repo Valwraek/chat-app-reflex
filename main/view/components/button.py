5d6557260396063bffb34a689f156f198be177de
Valwraek <bardiarajabirajabi@gmail.com>
2024-12-26 20:08:42 +0100

    feat(componente): :sparkles: Actualizar el componente button para tener estilos con css puro

--- main/view/components/button.py
+++ main/view/components/button.py
@@ -1,10 +1,9 @@
 import reflex as rx
-from ..styles import chat_style as cs
 from main.controller.pregunta_respuesta import State_qa as sqa
 
 def button(text="", on_click=None):
     return rx.button(
         f"{text}",
-        style=cs.button_style,
         on_click=on_click,
+        class_name="button"
     )
